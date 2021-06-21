2eff020350ab2c9061214c337b34877d7fc85061
stacy0412 <57612187+stacy0412@users.noreply.github.com>
2021-06-21 15:54:57 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,28 @@
+//* Add PayPal Email
+simpleCart({
+  checkout: {
+    type: "PayPal",
+    email: "you@yours.com"
+  }
+});
+
+//* Add shopping cart dropdown in header
+jQuery(document).ready(function () {
+  $('.showCart').on('click', function () {
+    $('#cartPopover').slideToggle('fast');
+    $('.showCart span.dropdown').toggleClass('fa-chevron-circle-down fa-chevron-circle-up');
+  })
+});
+
+//* Define spreadsheet URL (make sure you add the #gid=0 for the sheet you want to use)
+//* var googleSheetURI = 'https://docs.google.com/spreadsheets/d/13cAynY_PmTDpis1-zrdO4MGCTvxgJxs2dBAJss34muQ/edit#gid=138076371';
+var googleSheetURI = 'https://docs.google.com/spreadsheets/d/1_jtmni38-9eZBUCtHC8gtX405wPRhRSy_5TyTDecHe8/edit#gid=1804221129';
+//* Compile the Handlebars template for HR leaders
+var HRTemplate = Handlebars.compile($('#hr-template').html());
+
+//* Load products from spreadsheet
+$('#products').sheetrock({
+  url: googleSheetURI,
+  query: "select A,B,C,D,E",
+  rowTemplate: HRTemplate
+});
